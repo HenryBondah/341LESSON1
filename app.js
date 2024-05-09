85f3a656289b88464c66f4417b64e95cf094c0bf
HenryBondah <bondahdean@gmail.com>
2024-05-09 16:43:46 -0600

    update

--- app.js
+++ app.js
@@ -5,13 +5,19 @@ const mongodb = require('./db/connect');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app 
-  .use(bodyParser.json())
-  .use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next();
-  })
-  .use('/', require('./routes'));
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+// Redirect default route to contacts
+app.get('/', (req, res) => {
+  res.redirect('/contacts');
+});
+
+app.use('/contacts', require('./routes/contacts'));
+app.use('/', require('./routes'));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
@@ -21,4 +27,3 @@ mongodb.initDb((err, mongodb) => {
     console.log(`Connected to DB and listening on ${port}`);
   }
 });
-   
