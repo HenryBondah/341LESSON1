0ce1853e9aa58bd3c07cc0d92a96a5bea5e0e0d0
Henry Dean Bondah <122046340+HenryBondah@users.noreply.github.com>
2024-05-18 14:11:44 -0600

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-// routes/index.js
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger-output.json');
 
+router.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use('/contacts', require('./contacts'));
 
 module.exports = router;
