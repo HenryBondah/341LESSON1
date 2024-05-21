fa4e16bb68010629b7ca85c152dcb656cd17e4ce
Henry Dean Bondah <122046340+HenryBondah@users.noreply.github.com>
2024-05-21 10:59:28 -0600

    Update swagger.js

--- swagger.js
+++ swagger.js
@@ -3,10 +3,12 @@ const swaggerAutogen = require('swagger-autogen')();
 const doc = {
   info: {
     title: 'My API',
-    description: 'Contact API',
+    description: 'Contacts API',
   },
-  host: ['localhost:8080', 'https://three41lesson1.onrender.com'],
-  schemes: ['http', 'https'],
+  // host: 'localhost:8080/api-docs',
+  // schemes: ['http'],
+  host: 'three41lesson1.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
